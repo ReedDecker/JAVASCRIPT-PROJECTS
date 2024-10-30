669fb950d1fba5e0e32b27e8cfc5397e71ce9088
ReedDecker <reeddecker1@gmail.com>
2024-10-30 13:27:02 -0600

    Delete Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js

--- Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
+++
@@ -1,8 +0,0 @@
-var Sent1 = "I am a Rocket Man, ", Sent2 = "Watch me FLY!";
-document.write(Sent1 + Sent2) //This is using document.write to concatenate Sent1 and Sent2
-
-window.alert("Hello, Earthlings!"); //This is using window.alert to have a Pop-Up Window on browser page
-
-document.write("\u00A0 Hello" + ", " + "Earthlings"); //This is a String expression
-
-document.write(5 * 10) //This is an Arithmetic expression
