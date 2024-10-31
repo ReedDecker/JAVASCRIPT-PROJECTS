828a236b5cdc04315d6b45688b5b43c65ce99bf9
ReedDecker <reeddecker1@gmail.com>
2024-10-31 10:33:26 -0600

    Commit Project2_Functions Folder
    Commit Project2_Functions folder to JAVASCRIPT-PROJECTS GitHub repo for TTA review

---
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+
+///Creating functions using += operator to concatenate a string and .getElementId to call HTML "id"
+
+function myFunction2() {
+    var sentence = "A B C D E F G H I J K L M";
+    sentence += "N O P Q R S T U V W X Y Z";
+    document.getElementById("Alphabet").innerHTML = sentence;
+}
+
+///Creating functions using += operator to concatenate a string and .getElementId to call HTML "id"
+
